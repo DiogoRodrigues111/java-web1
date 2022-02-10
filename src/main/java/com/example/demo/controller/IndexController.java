b1ead9f305a642888a13b8698b07a7018d41cff7
Diogo R Roessler <drroessler@hotmail.com.br>
2022-02-10 04:01:46 -0300

    System have functional.

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -2,10 +2,15 @@ package com.example.demo.controller;
 
 import java.io.File;
 
-import org.springframework.lang.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.jpa.entity.User;
+import com.example.demo.jpa.services.UserService;
 
 /**
  * Index page controller
@@ -15,6 +20,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(path = "/", method = RequestMethod.POST)
+	public String writeIndexParams(
+			  @RequestParam("nome")  String nome
+			, @RequestParam("email") String email
+			, User user )
+	{		
+		user.setUserName(nome);
+		user.setEmail(email);
+		
+		userService.saveUsrData(user);
+		
+		return "index";
+	}
+	
 	/**
 	 * Index page of the HTML.
 	 *
