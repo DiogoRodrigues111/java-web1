612f93dd2b11c9443456481f9a89cb1eb70fae2e
Diogo R Roessler <drroessler@hotmail.com.br>
2023-04-21 20:14:38 -0300

    Added new commands and README

--- src/main/java/com/example/demo/exceptions/Exceptions.java
+++ src/main/java/com/example/demo/exceptions/Exceptions.java
@@ -1,5 +1,8 @@
 package com.example.demo.exceptions;
 
+/**
+ * Exception Handler
+ */
 public interface Exceptions {
     public static RuntimeException RUNTIME_EXCEPTION = new RuntimeException(":: EXCEPTION :: Runtime Occurr.");
     public static RuntimeException FAILED_LOGIN_EXCEPTION = new RuntimeException(":: EXCEPTION:: Failed to Login.");
